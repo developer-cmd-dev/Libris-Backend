ea62a600734bb65d641e0b45e158ed033cdba48d
developer-cmd-dev <devkmandal0@gmail.com>
2025-04-01 01:27:54 +0530

    Saved private keys in env files

---
+++ src/test/java/com/developerDev/Libris/Repository/BooksRepoImplTestWithDB.java
@@ -0,0 +1,24 @@
+package com.developerDev.Libris.Repository;
+
+import com.developerDev.Libris.JsonResposeEntity.BooksDataResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+@Slf4j
+@ExtendWith(MockitoExtension.class)
+public class BooksRepoImplTestWithDB
+}
